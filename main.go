023855bad74bb516c7a3ddf4e78120242be62ce1
nielsgal <galjaardniels@gmail.com>
2020-07-04 17:39:39 +0200

    formatted urls with - instead of concatenating

--- main.go
+++ main.go
@@ -7,23 +7,23 @@ import (
 
 
 func setupRoutes(app *fiber.App){
-	app.Get("/api/v1/getmenu",views.GetMenu)
-	app.Post("/api/v1/createmenu",views.CreateMenu)
-	app.Delete("/api/v1/deletemenu",views.DeleteMenu)
+	app.Get("/api/v1/get-menu",views.GetMenu)
+	app.Post("/api/v1/create-menu",views.CreateMenu)
+	app.Delete("/api/v1/delete-menu",views.DeleteMenu)
 
-	app.Get("/api/v1/getproduct",views.GetProduct)
-	app.Get("/api/v1/getproducts",views.GetProducts)
-	app.Post("/api/v1/createproduct",views.CreateProduct)
-	app.Post("/api/v1/createproducts",views.CreateProducts)
-	app.Delete("/api/v1/deleteproduct",views.DeleteProduct)
-	app.Delete("/api/v1/deleteproducts",views.DeleteProducts)
+	app.Get("/api/v1/get-product",views.GetProduct)
+	app.Get("/api/v1/get-products",views.GetProducts)
+	app.Post("/api/v1/create-product",views.CreateProduct)
+	app.Post("/api/v1/create-products",views.CreateProducts)
+	app.Delete("/api/v1/delete-product",views.DeleteProduct)
+	app.Delete("/api/v1/delete-products",views.DeleteProducts)
 
-	app.Get("/api/v1/getcategory",views.GetCategory)
-	app.Get("/api/v1/getcategories",views.GetCategories)
-	app.Post("/api/v1/createcategory",views.CreateCategory)
-	app.Post("/api/v1/createcategories",views.CreateCategories)
-	app.Delete("/api/v1/deletecategory",views.DeleteCategory)
-	app.Delete("/api/v1/deletecategories",views.DeleteCategories)
+	app.Get("/api/v1/get-category",views.GetCategory)
+	app.Get("/api/v1/get-categories",views.GetCategories)
+	app.Post("/api/v1/create-category",views.CreateCategory)
+	app.Post("/api/v1/create-categories",views.CreateCategories)
+	app.Delete("/api/v1/delete-category",views.DeleteCategory)
+	app.Delete("/api/v1/delete-categories",views.DeleteCategories)
 }
 
 
